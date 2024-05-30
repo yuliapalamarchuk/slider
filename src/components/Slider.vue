6d87efed3352a2336314eab5d3c6d37fadc973bb
Юлия Паламарчук <palamarchuk.yu@office.lan>
2024-05-30 19:30:33 +0300

    slides done

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -45,23 +45,62 @@
 
       <q-carousel-slide
         name="style"
-        class="column no-wrap flex-center custom-slide"
+        class="custom-slide-cosmology custom-slide"
       >
-        <h3>Кабинет Космобиологии</h3>
-        <div class="q-mt-md text-center">{{ lorem }}</div>
-        <img src="../assets/cosmobiology_img.png" alt="" />
+        <div class="custom-slide__wrapper">
+          <h3 class="custom-slide__title">Кабинет Космобиологии</h3>
+          <div class="q-mt-md text-left custom-slide__text">
+            Это настоящий оазис в Реянской школе. Здесь так много разнообразных
+            растений, что можно провести не один час, любуясь их красотой.Особое
+            восхищение вызывает «Purpura Великолепная». Растение с крупными
+            пурпурными цветами, которые источают нежный аромат.
+          </div>
+          <img src="../assets/cosmobiology_img.png" alt="" />
+        </div>
       </q-carousel-slide>
-      <q-carousel-slide name="tv" class="column no-wrap flex-center">
-        <q-icon name="live_tv" size="56px" />
-        <div class="q-mt-md text-center">{{ lorem }}</div>
+
+      <q-carousel-slide name="tv" class="custom-slide-history custom-slide">
+        <div class="custom-slide__wrapper">
+          <h3 class="custom-slide__title">Кабинет Межпланетной истории</h3>
+          <div class="q-mt-md text-left custom-slide__text">
+            Межпланетная история — это один из самых интересных и в то же время
+            сложных предметов в школе.  Амальтея настолько увлекается изучением
+            межпланетной истории, что может проводить в кабинете много времени.
+            После этого она с радостью делится с землянами интересными фактами,
+            которые узнала.
+          </div>
+          <img src="../assets/history_img.png" alt="" />
+        </div>
       </q-carousel-slide>
-      <q-carousel-slide name="layers" class="column no-wrap flex-center">
-        <q-icon name="layers" size="56px" />
-        <div class="q-mt-md text-center">{{ lorem }}</div>
+
+      <q-carousel-slide
+        name="layers"
+        class="custom-slide-astronomy custom-slide"
+      >
+        <div class="custom-slide__wrapper">
+          <h3 class="custom-slide__title">Кабинет Астрономии</h3>
+          <div class="q-mt-md text-left custom-slide__text">
+            Кабинет астрономии — это место, где проходят уроки по изучению
+            различных планет, звёзд и созвездий. Это любимый предмет Квази, он
+            часто засматривается на фотографию звезды Sirius, висящую на стене
+            возле его парты, размышляя о её характеристиках и особенностях.
+          </div>
+          <img src="../assets/astronomy_img.png" alt="" />
+        </div>
       </q-carousel-slide>
-      <q-carousel-slide name="map" class="column no-wrap flex-center">
-        <q-icon name="terrain" size="56px" />
-        <div class="q-mt-md text-center">{{ lorem }}</div>
+
+      <q-carousel-slide name="map" class="custom-slide-class custom-slide">
+        <div class="custom-slide__wrapper">
+          <h3 class="custom-slide__title">Учебный класс</h3>
+          <div class="q-mt-md text-left custom-slide__text">
+            Это кабинет, где проходят все остальные уроки. Здесь ученики изучают
+            различные предметы, которые не связаны с космосом. Главная
+            особенность учебного класса — это «ГалактикаТех». «ГалактикаТех»
+            включает в себя различные устройства и инструменты, которые помогают
+            ученикам получать новые знания и навыки.
+          </div>
+          <img src="../assets/class_img.png" alt="" />
+        </div>
       </q-carousel-slide>
     </q-carousel>
   </div>
@@ -81,10 +120,8 @@ export default {
 
     return {
       vertical,
-      navigation: ref(false), // Отключаем стандартную навигацию
+      navigation: ref(false),
       slide,
-      lorem:
-        "Это настоящий оазис в Реянской школе. Здесь так много разнообразных растений, что можно провести не один час, любуясь их красотой.Особое восхищение вызывает «Purpura Великолепная». Растение с крупными пурпурными цветами, которые источают нежный аромат.",
       goToSlide,
     };
   },
@@ -104,6 +141,30 @@ export default {
   overflow: visible;
 }
 
+.custom-slide__title {
+  font-family: "BrokenLed";
+  font-size: 44px;
+  font-weight: 400;
+  line-height: 48.4px;
+  text-align: left;
+  margin: 0;
+  padding-top: 50px;
+}
+
+.custom-slide__text {
+  font-family: "Geologica";
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 32px;
+  padding-top: 24px;
+  padding-bottom: 40px;
+}
+
+.custom-slide__wrapper {
+  max-width: 700px;
+  margin: 0 auto;
+}
+
 .custom-controls {
   position: absolute;
   left: 3rem;
@@ -119,9 +180,23 @@ export default {
 }
 
 .custom-slide {
-  background-image: url("../assets/cosmology.png");
   background-repeat: no-repeat;
   background-size: contain;
-  width: 1157px;
+  max-width: 1157px;
+}
+
+.custom-slide-cosmology {
+  background-image: url("../assets/cosmology.png");
+}
+
+.custom-slide-history {
+  background-image: url("../assets/history.png");
+}
+
+.custom-slide-astronomy {
+  background-image: url("../assets/astronomy.png");
+}
+.custom-slide-class {
+  background-image: url("../assets/class.png");
 }
 </style>
